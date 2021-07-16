8acfe9ead235561b85a44487ad2ac4849072fbf7
waseem1104 <waseem11@hotmail.fr>
2021-07-16 17:22:58 +0200

    Add class Router

--- oauth-client/index.php
+++ oauth-client/index.php
@@ -165,44 +165,9 @@ function getUser($params)
  * => Exchange Code <> Token (/auth-success)
  * => Exchange Token <> User info (/auth-success)
  */
-$route = strtok($_SERVER["REQUEST_URI"], "?");
-switch ($route) {
-    case '/login':
-        handleLogin();
-        break;
-    case '/auth-success':
-        handleSuccess();
-        break;
-    case '/fbauth-success':
-        handleFbSuccess();
-        break;
-    case '/discord-success':
-        handleDiscordSuccess();
-        break;
 
-    case '/github-success':
-        handleGithubSuccess();
-        break;
-    case '/auth-cancel':
-        handleError();
-        break;
-    case '/password':
-        if ($_SERVER['REQUEST_METHOD'] === "GET") {
-            echo '<form method="POST">';
-            echo '<input name="username">';
-            echo '<input name="password">';
-            echo '<input type="submit" value="Submit">';
-            echo '</form>';
-        } else {
-            ["username" => $username, "password" => $password] = $_POST;
-            getUser([
-                'grant_type' => "password",
-                "username" => $username,
-                "password" => $password
-            ]);
-        }
-        break;
-    default:
-        http_response_code(404);
-        break;
-}
+include "Core/Router.php";
+
+$route = strtok($_SERVER["REQUEST_URI"], "?");
+$router = new Router($route);
+$router->run();
